f1961518b26cb7e834b63169cfc16acceaa19eee
Jean-Pierre Bianchi <jpbianchi@gmail.com>
2023-11-30 17:31:46 +0100

    added llm_prompt to fastapi.py

--- fastapi.py
+++ fastapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import modal
 
 app = FastAPI()
 
@@ -21,3 +22,10 @@ async def create_item(item: Item):
 @app.get("/items/{item_name}")
 async def read_item(item_name: str):
     return items.get(item_name)
+
+@app.post("/llm_prompt/")
+async def llm_prompt(prompt: str = "What is your name?", modelname: str ="01-ai/Yi-6B"):
+
+    f = modal.Function.lookup("GPU_server", "llm_prompt")
+    answer = f.remote(modelname, prompt)
+    return {"answer": answer}
