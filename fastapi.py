3041779885749cc465eaae01b0efa66907de55da
Jean-Pierre Bianchi <jpbianchi@gmail.com>
2023-11-30 16:41:27 +0100

    FAST api

---
+++ fastapi.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# A simple model to structure the data you receive
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+# In-memory storage for demonstration
+items = {}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    items[item.name] = item
+    return item
+
+@app.get("/items/{item_name}")
+async def read_item(item_name: str):
+    return items.get(item_name)
